7b851565d2eaa3bf76e347844f944b2de789cf54
Денис Макаров <denis@MacBook-Pro-Denis.local>
2018-03-10 18:41:47 +0300

    add terms with index

--- index.php
+++ index.php
@@ -2,6 +2,40 @@
 
 require __DIR__ . '/Stemmer.php';
 
+/***************** OUTPUT TERMS ********************/
+
+/* CLASSES FOR OUTPUT TERMS ARRAY */
+class Term {
+    public $name;
+    public $count;
+    public $index;
+}
+
+/* FUNCTIONS FOR OUTPUT TERMS ARRAY */
+function get_num_of_words($string) {
+    // $string = preg_replace('/\s+/', ' ', trim($string));
+    $words = explode(" ", $string);
+
+    return count($words);
+}
+
+function get_num_of_article($key, $documents) {
+	$documentNumber = 1;
+	foreach ($documents as $document) {
+		$wordsAmount = $wordsAmount + get_num_of_words($document);
+		if ($wordsAmount >= $key) {
+			break;
+		}
+		else {
+			$documentNumber++;
+		}
+	}
+
+	return $documentNumber;
+}
+
+/***************** OUTPUT ARTICLES ********************/
+
 /* CLASSES FOR OUTPUT FILE */
 class Article {
     public $link;
@@ -51,13 +85,14 @@ function getKeywordsByLink($doc) {
 	return $element[0]->nodeValue;
 }
 
-/* FUNCTION FOR MYSTEM LAUNCH */
 function mystem($q) {
   $result = exec('echo "'.$q.'" | ./mystem -l -d');
   return $result;
 }
 
-/* MAIN PART (EXTRACT INFORMATION FROM WEBSITE) */
+
+/************** MAIN PART (EXTRACT INFORMATION FROM WEBSITE) ***************/
+
 $url = 'http://www.mathnet.ru/php/archive.phtml?jrnid=uzku&wshow=issue&bshow=contents&series=0&year=2014&volume=156&issue=1&option_lang=rus&bookID=1517';
 
 libxml_use_internal_errors(true); 
@@ -127,11 +162,287 @@ foreach ($elements as $element) {
 	$index++;
 }
 
-/* GENERATE JSON FILE */
+/* GENERATE ARTICLES JSON FILE */
 
 $json = json_encode($root, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 echo $json;
 
 file_put_contents('/Users/denis/Documents/Search Class/myfile.json', $json);
+
+
+/************** MAIN PART (OUTPUT TERMS INDEXES) ***************/
+
+/* PORTER TITLE */
+
+$strings = array();
+foreach ($root->articles as $article) {
+	array_push($strings, $article->title->porter);
+}
+
+$commonString = " ";
+foreach ($strings as $key => $string) {
+	$strings[$key] = mb_strtolower($strings[$key]);
+	$strings[$key] = preg_replace('/\$[^$]*\$/', '', $strings[$key]);
+	$strings[$key] = str_replace('”', '', $strings[$key]);
+	$strings[$key] = str_replace('“', '', $strings[$key]);
+	$strings[$key] = str_replace(':', '', $strings[$key]);
+	$strings[$key] = str_replace('.', '', $strings[$key]);
+	$strings[$key] = str_replace(',', '', $strings[$key]);
+	$strings[$key] = str_replace(';', '', $strings[$key]);
+	$strings[$key] = str_replace('|', '', $strings[$key]);
+	$strings[$key] = str_replace('[', '', $strings[$key]);
+	$strings[$key] = str_replace(']', '', $strings[$key]);
+	$strings[$key] = str_replace('(', '', $strings[$key]);
+	$strings[$key] = str_replace(')', '', $strings[$key]);
+	$strings[$key] = str_replace(' ', ' ', $strings[$key]);
+
+	$commonString = $commonString . $strings[$key] . " ";
+}
+
+$words = explode(" ", $commonString);
+
+for ($index=0;$index<count($words);$index++) {
+	$word = $words[$index];
+	if ($word != "") {
+		$amountOfWord = substr_count($commonString, " " . $word . " ");
+	
+		$titleResult->words[$index]->text = $word;
+		$indexes = array();
+
+		for ($i=0;$i<$amountOfWord;$i++) {
+			$key = array_search($word, $words);
+			$words[$key] = "";
+			array_push($indexes, get_num_of_article($key, $strings));
+		}
+
+		$indexes = array_unique($indexes);
+		$newIndexes = array_values($indexes);
+
+		$titleResult->words[$index]->count = count($newIndexes);
+		$titleResult->words[$index]->index = $newIndexes;
+	}
+
+}
+
+usort($titleResult->words, function($a, $b) { // Sort the array using a user defined function
+    return $a->text < $b->text ? -1 : 1; //Compare the scores
+});   
+
+
+/* PORTER ABSTRACT */
+
+$strings = array();
+foreach ($root->articles as $article) {
+	array_push($strings, $article->abstract->porter);
+}
+
+$commonString = " ";
+foreach ($strings as $key => $string) {
+	$strings[$key] = mb_strtolower($strings[$key]);
+	$strings[$key] = preg_replace('/\$[^$]*\$/', '', $strings[$key]);
+	$strings[$key] = str_replace('”', '', $strings[$key]);
+	$strings[$key] = str_replace('“', '', $strings[$key]);
+	$strings[$key] = str_replace(':', '', $strings[$key]);
+	$strings[$key] = str_replace('.', '', $strings[$key]);
+	$strings[$key] = str_replace(',', '', $strings[$key]);
+	$strings[$key] = str_replace(';', '', $strings[$key]);
+	$strings[$key] = str_replace('|', '', $strings[$key]);
+	$strings[$key] = str_replace('[', '', $strings[$key]);
+	$strings[$key] = str_replace(']', '', $strings[$key]);
+	$strings[$key] = str_replace('(', '', $strings[$key]);
+	$strings[$key] = str_replace(')', '', $strings[$key]);
+	$strings[$key] = str_replace(' ', ' ', $strings[$key]);
+
+	$commonString = $commonString . $strings[$key] . " ";
+}
+
+$words = explode(" ", $commonString);
+$result = "";
+
+for ($index=0;$index<count($words);$index++) {
+	$word = $words[$index];
+	if ($word != "") {
+		$amountOfWord = substr_count($commonString, " " . $word . " ");
+	
+		$abstractResult->words[$index]->text = $word;
+		$indexes = array();
+
+		for ($i=0;$i<$amountOfWord;$i++) {
+			$key = array_search($word, $words);
+			$words[$key] = "";
+			array_push($indexes, get_num_of_article($key, $strings));
+		}
+
+		$indexes = array_unique($indexes);
+		$newIndexes = array_values($indexes);
+
+		$abstractResult->words[$index]->count = count($newIndexes);
+		$abstractResult->words[$index]->index = $newIndexes;
+	}
+
+}
+
+usort($abstractResult->words, function($a, $b) { // Sort the array using a user defined function
+    return $a->text < $b->text ? -1 : 1; //Compare the scores
+});  
+
+/* MYSTEM TITLE */
+
+$titleResult = "";
+
+$strings = array();
+foreach ($root->articles as $article) {
+	array_push($strings, $article->title->mystem);
+}
+
+$commonString = " ";
+foreach ($strings as $key => $string) {
+	$strings[$key] = mb_strtolower($strings[$key]);
+	$strings[$key] = preg_replace('/\$[^$]*\$/', '', $strings[$key]);
+	$strings[$key] = str_replace('”', '', $strings[$key]);
+	$strings[$key] = str_replace('“', '', $strings[$key]);
+	$strings[$key] = str_replace(':', '', $strings[$key]);
+	$strings[$key] = str_replace('.', '', $strings[$key]);
+	$strings[$key] = str_replace(',', '', $strings[$key]);
+	$strings[$key] = str_replace(';', '', $strings[$key]);
+	$strings[$key] = str_replace('|', '', $strings[$key]);
+	$strings[$key] = str_replace('[', '', $strings[$key]);
+	$strings[$key] = str_replace(']', '', $strings[$key]);
+	$strings[$key] = str_replace('(', '', $strings[$key]);
+	$strings[$key] = str_replace(')', '', $strings[$key]);
+	$strings[$key] = str_replace(' ', ' ', $strings[$key]);
+
+	$commonString = $commonString . $strings[$key] . " ";
+}
+
+$words = explode(" ", $commonString);
+$result = "";
+
+for ($index=0;$index<count($words);$index++) {
+	$word = $words[$index];
+	if ($word != "") {
+		$amountOfWord = substr_count($commonString, " " . $word . " ");
+	
+		$mystemTitleResult->words[$index]->text = $word;
+		$indexes = array();
+
+		for ($i=0;$i<$amountOfWord;$i++) {
+			$key = array_search($word, $words);
+			$words[$key] = "";
+			array_push($indexes, get_num_of_article($key, $strings));
+		}
+
+		$indexes = array_unique($indexes);
+		$newIndexes = array_values($indexes);
+
+		$mystemTitleResult->words[$index]->count = count($newIndexes);
+		$mystemTitleResult->words[$index]->index = $newIndexes;
+	}
+
+}
+
+usort($mystemTitleResult->words, function($a, $b) { // Sort the array using a user defined function
+    return $a->text < $b->text ? -1 : 1; //Compare the scores
+});    
+
+/* MYSTEM ABSTRACT */
+
+$strings = array();
+foreach ($root->articles as $article) {
+	array_push($strings, $article->abstract->mystem);
+}
+
+$commonString = " ";
+foreach ($strings as $key => $string) {
+	$strings[$key] = mb_strtolower($strings[$key]);
+	$strings[$key] = preg_replace('/\$[^$]*\$/', '', $strings[$key]);
+	$strings[$key] = str_replace('”', '', $strings[$key]);
+	$strings[$key] = str_replace('“', '', $strings[$key]);
+	$strings[$key] = str_replace(':', '', $strings[$key]);
+	$strings[$key] = str_replace('.', '', $strings[$key]);
+	$strings[$key] = str_replace(',', '', $strings[$key]);
+	$strings[$key] = str_replace(';', '', $strings[$key]);
+	$strings[$key] = str_replace('|', '', $strings[$key]);
+	$strings[$key] = str_replace('[', '', $strings[$key]);
+	$strings[$key] = str_replace(']', '', $strings[$key]);
+	$strings[$key] = str_replace('(', '', $strings[$key]);
+	$strings[$key] = str_replace(')', '', $strings[$key]);
+	$strings[$key] = str_replace(' ', ' ', $strings[$key]);
+
+	$commonString = $commonString . $strings[$key] . " ";
+}
+
+$words = explode(" ", $commonString);
+$mystemAbstractResult = "";
+
+for ($index=0;$index<count($words);$index++) {
+	$word = $words[$index];
+	if ($word != "") {
+		$amountOfWord = substr_count($commonString, " " . $word . " ");
+	
+		$mystemAbstractResult->words[$index]->text = $word;
+		$indexes = array();
+
+		for ($i=0;$i<$amountOfWord;$i++) {
+			$key = array_search($word, $words);
+			$words[$key] = "";
+			array_push($indexes, get_num_of_article($key, $strings));
+		}
+
+		$indexes = array_unique($indexes);
+		$newIndexes = array_values($indexes);
+
+		$mystemAbstractResult->words[$index]->count = count($newIndexes);
+		$mystemAbstractResult->words[$index]->index = $newIndexes;
+	}
+
+}
+
+usort($mystemAbstractResult->words, function($a, $b) { // Sort the array using a user defined function
+    return $a->text < $b->text ? -1 : 1; //Compare the scores
+});   
+
+
+$index = 0;
+foreach ($titleResult->words as $key => $term) {
+	$terms->porter->title->terms[$index] = new Term();
+	$terms->porter->title->terms[$index]->name = $term->text;
+	$terms->porter->title->terms[$index]->count = $term->count;
+	$terms->porter->title->terms[$index]->index = $term->index;
+	$index++;
+}
+
+$index = 0;
+foreach ($abstractResult->words as $key => $term) {
+	$terms->porter->abstract->terms[$index] = new Term();
+	$terms->porter->abstract->terms[$index]->name = $term->text;
+	$terms->porter->abstract->terms[$index]->count = $term->count;
+	$terms->porter->abstract->terms[$index]->index = $term->index;
+	$index++;
+}
+
+$index = 0;
+foreach ($mystemTitleResult->words as $key => $term) {
+	$terms->mystem->title->terms[$index] = new Term();
+	$terms->mystem->title->terms[$index]->name = $term->text;
+	$terms->mystem->title->terms[$index]->count = $term->count;
+	$terms->mystem->title->terms[$index]->index = $term->index;
+	$index++;
+}
+
+$index = 0;
+foreach ($mystemAbstractResult->words as $key => $term) {
+	$terms->mystem->abstract->terms[$index] = new Term();
+	$terms->mystem->abstract->terms[$index]->name = $term->text;
+	$terms->mystem->abstract->terms[$index]->count = $term->count;
+	$terms->mystem->abstract->terms[$index]->index = $term->index;
+	$index++;
+}
+
+$listJson = json_encode($terms, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+echo $listJson;
+
+file_put_contents('/Users/denis/Documents/Search Class/myTermsList.json', $listJson);
+
 
 ?>
